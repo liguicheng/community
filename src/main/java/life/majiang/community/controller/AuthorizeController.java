0a46c84e4185c7c994ff6412c8c513f8b03821a7
unknown <1098668564@qq.com>
2020-03-05 22:04:38 +0800

    登录显示

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
     
@@ -26,7 +28,8 @@ public class AuthorizeController {
     
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state) {
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -37,9 +40,11 @@ public class AuthorizeController {
         GithubUser user = githubProvider.getUser(accessToken);
         if(user != null) {
             // 登录成功，写cookie和session
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
         }else {
             //登录失败，重新登录
+            return "redirect:/";
         }
-        return "index";
     }
 }
